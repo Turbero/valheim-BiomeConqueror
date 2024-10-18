90091edcb97cb72098d1c4c5eae24d0b366add58
Turbero <dicon18@gmail.com>
2024-10-19 01:35:07 +0200

    Updating descriptions

--- BiomeConqueror/BiomeConqueror/ConfigurationFile.cs
+++ BiomeConqueror/BiomeConqueror/ConfigurationFile.cs
@@ -21,8 +21,8 @@ namespace BiomeConqueror
 
                 modEnabled = config.Bind<bool>("1 - General", "Enabled", true, "Enabling/Disabling the mod (default = true)");
                 debug = config.Bind<bool>("1 - General", "DebugMode", false, "Enabling/Disabling the debugging in the console (default = false)");
-                bonemassBenefitEnabled = config.Bind<bool>("2 - Victories", "BonemassBenefitEnabled", true, "Disable rain in all swamps after killing Bonemass (default = true)");
-                moderBenefitEnabled = config.Bind<bool>("2 - Victories", "ModerBenefitEnabled", true, "Disable freezing in all mountains after killing Moder (default = true)");
+                bonemassBenefitEnabled = config.Bind<bool>("2 - Victories", "BonemassBenefitEnabled", true, "Stops getting wet by rain in all swamps after killing Bonemass (default = true)");
+                moderBenefitEnabled = config.Bind<bool>("2 - Victories", "ModerBenefitEnabled", true, "Stops freezing without protection effects in all mountains after killing Moder (default = true)");
                 queenBenefitEnabled = config.Bind<bool>("2 - Victories", "QueenBenefitEnabled", true, "Increases the wisp light range after killing The Seeker Queen (default = true)");
                 queenBenefitRange = config.Bind<float>("2 - Victories", "QueenBenefitRange", 100f, "Establishes the new wisp light range after killing The Seeker Queen (default = true)");
             }
