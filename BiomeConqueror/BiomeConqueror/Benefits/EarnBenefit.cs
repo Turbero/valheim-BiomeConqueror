586d02a0196f0eac6afb5971e86a8869bddbcfac
Turbero <dicon18@gmail.com>
2024-11-13 00:24:44 +0100

    Only counting when boss is killed in his biome

--- BiomeConqueror/BiomeConqueror/Benefits/EarnBenefit.cs
+++ BiomeConqueror/BiomeConqueror/Benefits/EarnBenefit.cs
@@ -16,43 +16,41 @@ namespace BiomeConqueror.Benefits
                 if (__instance != null && __instance.IsBoss())
                 {
                     Logger.Log($"defeated name: {__instance.m_name}");
-                    if (__instance.m_name == "$enemy_bonemass")
+                    if (__instance.m_name == "$enemy_bonemass" && Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Swamp)
                     {
                         Player.m_localPlayer.AddUniqueKey(BONEMASS_DEFEATED_PLAYER_KEY);
                         Logger.Log($"** Bonemass defeated");
-                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.bonemassBenefitEligibleEnabled.Value &&
-                            Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Swamp)
+                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.bonemassBenefitEligibleEnabled.Value)
                             PlayerBuffs.AddBenefitBuff(Player.m_localPlayer, BONEMASS_DEFEATED_MESSAGE_KEY, TROPHY_BONEMASS);
                     }
-                    else if (__instance.m_name == "$enemy_dragon")
+                    else if (__instance.m_name == "$enemy_dragon" && Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Mountain)
                     {
                         Player.m_localPlayer.AddUniqueKey(MODER_DEFEATED_PLAYER_KEY);
                         Logger.Log($"** Moder defeated");
-                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.moderBenefitEligibleEnabled.Value &&
-                            Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Mountain)
+                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.moderBenefitEligibleEnabled.Value)
                             PlayerBuffs.AddBenefitBuff(Player.m_localPlayer, MODER_DEFEATED_MESSAGE_KEY, TROPHY_MODER);
                     }
-                    else if (__instance.m_name == "$enemy_goblinking")
+                    else if (__instance.m_name == "$enemy_goblinking" && Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Plains)
                     {
                         Player.m_localPlayer.AddUniqueKey(YAGLUTH_DEFEATED_PLAYER_KEY);
                         Logger.Log($"** Yagluth defeated");
-                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.yagluthBenefitEligibleEnabled.Value &&
-                            Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Plains)
+                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.yagluthBenefitEligibleEnabled.Value)
                             PlayerBuffs.AddBenefitBuff(Player.m_localPlayer, YAGLUTH_DEFEATED_MESSAGE_KEY, TROPHY_YAGLUTH);
                     }
-                    else if (__instance.m_name == "$enemy_seekerqueen")
+                    else if (__instance.m_name == "$enemy_seekerqueen" && Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Mistlands)
                     {
                         Player.m_localPlayer.AddUniqueKey(QUEEN_DEFEATED_PLAYER_KEY);
                         Logger.Log($"** Queen defeated");
-                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.queenBenefitEligibleEnabled.Value &&
-                            Player.m_localPlayer.GetCurrentBiome() == Heightmap.Biome.Mistlands)
+                        if (ConfigurationFile.benefitIcons.Value && ConfigurationFile.queenBenefitEligibleEnabled.Value)
+                        {
                             PlayerBuffs.AddBenefitBuff(Player.m_localPlayer, QUEEN_DEFEATED_MESSAGE_KEY, TROPHY_QUEEN);
 
-                        var itemData = Player.m_localPlayer.GetInventory().GetEquippedItems().FirstOrDefault(i => i.m_dropPrefab.name == "Demister");
-                        if (itemData != null)
-                        {
-                            Player.m_localPlayer.UnequipItem(itemData);
-                            Player.m_localPlayer.EquipItem(itemData);
+                            var itemData = Player.m_localPlayer.GetInventory().GetEquippedItems().FirstOrDefault(i => i.m_dropPrefab.name == "Demister");
+                            if (itemData != null)
+                            {
+                                Player.m_localPlayer.UnequipItem(itemData);
+                                Player.m_localPlayer.EquipItem(itemData);
+                            }
                         }
                     }
                 }
